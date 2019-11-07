8c6ee28b607f155cce30257fe8b8ef53d7150598
Kahlil Hodgson <tartansandal@users.noreply.github.com>
2019-11-07 13:31:18 +1100

    Load completion library via env activation scripts

---
+++ activate.sh
@@ -0,0 +1,4 @@
+# Activation script for the bash-completion package
+
+unset BASH_COMPLETION_VERSINFO
+source /opt/anaconda1anaconda2anaconda3/etc/profile.d/bash_completion.sh
---
+++ deactivate.sh
@@ -0,0 +1,4 @@
+# Deactivation script for the bash-completion package
+#
+# Currently no sensible way to deactivate the bash completion facillity
+# This is just a place-holder
