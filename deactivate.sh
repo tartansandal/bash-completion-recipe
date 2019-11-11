7eb3750fd4504187bf4fdf8694b3a5bf4b822a84
Kahlil Hodgson <tartansandal@users.noreply.github.com>
2019-11-11 12:32:15 +1100

    Unset library loaded var on deactivate
    Not perfect, but this will at lease allow other envs to force a reload
    of the libray.

--- deactivate.sh
+++ deactivate.sh
@@ -1,4 +1,5 @@
 # Deactivation script for the bash-completion package
 #
-# Currently no sensible way to deactivate the bash completion facillity
-# This is just a place-holder
+# Currently no sensible way to fully remove the bash completion facillity from the users
+# shell. The following will at least allow for a reload.
+unset CONDA_BASH_COMPLETION_LOADED
